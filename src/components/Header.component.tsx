7e840c254a44a13260721f3456100d833cf17b6f
Ch1py7 <garcialopezgerardo6612@gmail.com>
2023-06-09 20:50:17 -0600

    chore: add eslint rules, refactor header and add todo types

--- src/components/Header.component.tsx
+++ src/components/Header.component.tsx
@@ -1,11 +1,14 @@
-import { FC, ReactElement } from 'react'
 import { Icon } from '@iconify/react'
+import { FC, ReactElement } from 'react'
 
 export const Header: FC = (): ReactElement => {
   return (
     <header className='flex justify-between py-9 px-16 bg-[#B892FF]'>
       <h1 className='flex items-center gap-x-4'>
-        <Icon style={{ fontSize: '5rem', color: 'purple' }} icon='ri:calendar-todo-fill' />
+        <Icon
+          style={{ fontSize: '5rem', color: 'purple' }}
+          icon='ri:calendar-todo-fill'
+        />
         <span className='text-6xl font-medium'>Whispering Goals</span>
       </h1>
       <nav className='flex items-center gap-x-9'>
@@ -19,3 +22,4 @@ export const Header: FC = (): ReactElement => {
     </header>
   )
 }
+
